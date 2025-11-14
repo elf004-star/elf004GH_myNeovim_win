2af5793594b0a77cb1c75aaa13ecea5010660dbb
elf004 <1873475824@qq.com>
2025-11-14 21:40:36 +0800

    安装4个插件

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "clangd",
+      },
+    },
+    opts_extend = { "ensure_installed" },
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require("mason-registry")
+
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end
+      if mr.refresh then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
+      end
+    end,
+  },
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = { 'saghen/blink.cmp' },
+    
+   -- example calling setup directly for each LSP
+    config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+  
+      vim.lsp.config('lua_ls', { capabilities = capabilities })
+      vim.lsp.enable('lua_ls')
+    end
+  }
+}
+
