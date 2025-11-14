e7fdd4a918d4d8c4eb975d8bce2551d014a452ab
elf004 <1873475824@qq.com>
2025-11-15 01:14:56 +0800

    补全初步完成

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,55 +1,106 @@
 return {
-    {
   'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  -- dependencies = { 'rafamadriz/friendly-snippets' },
-
-  -- use a release tag to download pre-built binaries
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+    'williamboman/mason.nvim',
+  },
+  event = { 'BufReadPost', 'BufNewFile' },
   version = '1.*',
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
   -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
-
-  ---@module 'blink.cmp'
-  ---@type blink.cmp.Config
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = { preset = 'default' },
-
     appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = 'mono'
     },
-
-    -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    completion = {
+      documentation = { auto_show = false }
+    },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
+      providers = {
+        snippets = { score_offset = 1000 },
+      },
     },
-
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" }
+    signature = {
+      enabled = true,
+    },
+    cmdline = {
+      completion = {
+        menu = {
+          auto_show = true,
+        },
+      },
+    },
   },
-  opts_extend = { "sources.default" }
-}
+  opts_extend = { "sources.default" },
+
+  config = function(_, opts)
+    -- Setup blink.cmp
+    require('blink.cmp').setup(opts)
+
+    -- LSP Configuration using Neovim 0.11+ built-in vim.lsp.config API
+    -- No nvim-lspconfig plugin needed!
+
+    -- Configure diagnostics
+    vim.diagnostic.config({
+      underline = false,
+      signs = false,
+      update_in_insert = false,
+      virtual_text = { spacing = 2, prefix = "●" },
+      severity_sort = true,
+      float = {
+        border = "rounded",
+      },
+    })
+
+    -- Setup keymaps when LSP attaches
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        -- vim.keymap.set("n", "K", vim.lsp.buf.hover) -- configured in "nvim-ufo" plugin
+        vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {
+          buffer = ev.buf,
+          desc = "[LSP] Show diagnostic",
+        })
+        vim.keymap.set("n", "<leader>gk", vim.lsp.buf.signature_help, { desc = "[LSP] Signature help" })
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "[LSP] Add workspace folder" })
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "[LSP] Remove workspace folder" })
+        vim.keymap.set("n", "<leader>wl", function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, { desc = "[LSP] List workspace folders" })
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "[LSP] Rename" })
+      end,
+    })
+
+    -- Get LSP capabilities from blink.cmp
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+    -- Configure language servers
+    vim.lsp.config('lua_ls', {
+      cmd = { 'lua-language-server' },
+      root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git' },
+      capabilities = capabilities,
+    })
+
+    vim.lsp.config('clangd', {
+      cmd = { 'clangd' },
+      root_markers = { '.clangd', '.clang-tidy', '.clang-format', 'compile_commands.json', 'compile_flags.txt', 'configure.ac', '.git' },
+      capabilities = capabilities,
+    })
+
+    -- Auto-enable LSP servers for appropriate filetypes
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'lua' },
+      callback = function()
+        vim.lsp.enable('lua_ls')
+      end,
+    })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+      callback = function()
+        vim.lsp.enable('clangd')
+      end,
+    })
+  end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,75 +1,30 @@
+-- Mason: LSP/DAP/Linter/Formatter package manager
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "lua-language-server",
-        "clangd",
-      },
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "lua-language-server",
+      "clangd",
     },
-    opts_extend = { "ensure_installed" },
-    config = function(_, opts)
-      require("mason").setup(opts)
-      local mr = require("mason-registry")
+  },
+  opts_extend = { "ensure_installed" },
+  config = function(_, opts)
+    require("mason").setup(opts)
+    local mr = require("mason-registry")
 
-      local function ensure_installed()
-        for _, tool in ipairs(opts.ensure_installed) do
-          local p = mr.get_package(tool)
-          if not p:is_installed() then
-            p:install()
-          end
+    local function ensure_installed()
+      for _, tool in ipairs(opts.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
         end
       end
-      if mr.refresh then
-        mr.refresh(ensure_installed)
-      else
-        ensure_installed()
-      end
-    end,
-  },
-  
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "saghen/blink.cmp", "williamboman/mason.nvim" },
-
-    -- example calling setup directly for each LSP
-    config = function()
-      vim.diagnostic.config({
-        underline = false,
-        signs = false,
-        update_in_insert = false,
-        virtual_text = { spacing = 2, prefix = "●" },
-        severity_sort = true,
-        float = {
-          border = "rounded",
-        },
-      })
-
-      -- Use LspAttach autocommand to only map the following keys
-      -- after the language server attaches to the current buffer
-      vim.api.nvim_create_autocmd("LspAttach", {
-        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-        callback = function(ev)
-          -- vim.keymap.set("n", "K", vim.lsp.buf.hover) -- configured in "nvim-ufo" plugin
-          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {
-            buffer = ev.buf,
-            desc = "[LSP] Show diagnostic",
-          })
-          vim.keymap.set("n", "<leader>gk", vim.lsp.buf.signature_help, { desc = "[LSP] Signature help" })
-          vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "[LSP] Add workspace folder" })
-          vim.keymap.set(
-            "n",
-            "<leader>wr",
-            vim.lsp.buf.remove_workspace_folder,
-            { desc = "[LSP] Remove workspace folder" }
-          )
-          vim.keymap.set("n", "<leader>wl", function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, { desc = "[LSP] List workspace folders" })
-          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "[LSP] Rename" })
-        end,
-      })
-    end,
-  },
+    end
+    if mr.refresh then
+      mr.refresh(ensure_installed)
+    else
+      ensure_installed()
+    end
+  end,
 }
 
