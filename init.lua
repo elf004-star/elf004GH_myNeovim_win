46da521a2e57dc4f311a4adf81d3b57fed3c4ad6
elf004 <1873475824@qq.com>
2025-11-14 22:19:54 +0800

    配置完成

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.opt.number=true
 vim.wo.cursorline = true
 -- Display tabs and trailing spaces
 vim.opt.list = true
@@ -6,6 +7,21 @@ vim.opt.listchars = { tab = ">-", trail = "-"}
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is  also a good place to setup other settings (vim.opt)
+
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+-- vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+-- vim.opt.autoindent = true
+-- vim.opt.smarttab = true
+-- vim.opt.smartcase = true
+-- vim.opt.ignorecase = true
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -27,17 +27,49 @@ return {
       end
     end,
   },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = { 'saghen/blink.cmp' },
-    
-   -- example calling setup directly for each LSP
-    config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
   
-      vim.lsp.config('lua_ls', { capabilities = capabilities })
-      vim.lsp.enable('lua_ls')
-    end
-  }
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "saghen/blink.cmp", "williamboman/mason.nvim" },
+
+    -- example calling setup directly for each LSP
+    config = function()
+      vim.diagnostic.config({
+        underline = false,
+        signs = false,
+        update_in_insert = false,
+        virtual_text = { spacing = 2, prefix = "●" },
+        severity_sort = true,
+        float = {
+          border = "rounded",
+        },
+      })
+
+      -- Use LspAttach autocommand to only map the following keys
+      -- after the language server attaches to the current buffer
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+          -- vim.keymap.set("n", "K", vim.lsp.buf.hover) -- configured in "nvim-ufo" plugin
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, {
+            buffer = ev.buf,
+            desc = "[LSP] Show diagnostic",
+          })
+          vim.keymap.set("n", "<leader>gk", vim.lsp.buf.signature_help, { desc = "[LSP] Signature help" })
+          vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "[LSP] Add workspace folder" })
+          vim.keymap.set(
+            "n",
+            "<leader>wr",
+            vim.lsp.buf.remove_workspace_folder,
+            { desc = "[LSP] Remove workspace folder" }
+          )
+          vim.keymap.set("n", "<leader>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, { desc = "[LSP] List workspace folders" })
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "[LSP] Rename" })
+        end,
+      })
+    end,
+  },
 }
 
