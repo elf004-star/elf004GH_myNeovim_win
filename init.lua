670011f15be6daf8b7644b259d5c3227a6545d40
elf004 <1873475824@qq.com>
2025-11-15 23:09:45 +0800

    界面美化

--- init.lua
+++ init.lua
@@ -1,28 +1,41 @@
-vim.opt.number=true
+-- disable netrw at the very start of your init.lua (因为使用nvim-tree)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.opt.number = true
 vim.wo.cursorline = true
 -- Display tabs and trailing spaces
 vim.opt.list = true
-vim.opt.listchars = { tab = ">-", trail = "-"}
+vim.opt.listchars = { tab = ">-", trail = "-" }
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
 
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is  also a good place to setup other settings (vim.opt)
+vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 10
+vim.opt.startofline = false
 
+vim.opt.conceallevel = 2
+
+vim.o.signcolumn = "yes:1"
+
+vim.wo.wrap = false
+
+-- Tab related options
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
--- vim.opt.tabstop = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
--- vim.opt.autoindent = true
--- vim.opt.smarttab = true
--- vim.opt.smartcase = true
--- vim.opt.ignorecase = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-require("config.lazy")  -- (C:\Users\Allen\AppData\Local\nvim/lua/config/Lazy.lua)
+require("config.lazy") -- Import `./lua/config/lazy.lua`
+
+require("keymapping")
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,195 @@
+return {
+  -- 配色方案：catppuccin.nvim
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      custom_highlights = function(colors)
+        -- stylua: ignore
+        return {
+          LineNr     = { fg = colors.surface2 },
+          Visual     = { bg = colors.overlay0 },
+          Search     = { bg = colors.surface2 },
+          IncSearch  = { bg = colors.mauve },
+          CurSearch  = { bg = colors.mauve },
+          MatchParen = { bg = colors.mauve, fg = colors.base, bold = true },
+        }
+      end,
+      integrations = {
+        barbar = true,
+        lualine = true,
+        nvimtree = true,
+        rainbow_delimiters = true,
+        noice = true,
+      },
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+
+  -- 状态栏：lualine.nvim
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      options = {
+        theme = "catppuccin",
+        always_divide_middle = false,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = {},
+        lualine_y = { "encoding", "fileformat", "filetype", "progress" },
+        lualine_z = { "location" },
+      },
+      -- stylua: ignore
+      winbar = {
+        lualine_a = { "filename", },
+        lualine_b = { { function() return " " end, color = "Comment", }, },
+        lualine_x = { "lsp_status", },
+      },
+      inactive_winbar = {
+        -- Always show winbar
+        -- stylua: ignore
+        lualine_b = { function() return " " end, },
+      },
+    },
+    config = function(_, opts)
+      require("lualine").setup(opts)
+    end,
+  },
+
+  -- 标签栏：barbar.nvim
+  {
+    "romgrk/barbar.nvim",
+    version = "^1.0.0",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    lazy = false,
+    -- stylua: ignore
+    keys = {
+      { "<A-<>", "<CMD>BufferMovePrevious<CR>", mode = {"n"}, desc = "[Buffer] Move buffer left"  },
+      { "<A->>", "<CMD>BufferMoveNext<CR>",     mode = {"n"}, desc = "[Buffer] Move buffer right" },
+      { "<A-1>", "<CMD>BufferGoto 1<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 1"    },
+      { "<A-2>", "<CMD>BufferGoto 2<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 2"    },
+      { "<A-3>", "<CMD>BufferGoto 3<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 3"    },
+      { "<A-4>", "<CMD>BufferGoto 4<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 4"    },
+      { "<A-5>", "<CMD>BufferGoto 5<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 5"    },
+      { "<A-6>", "<CMD>BufferGoto 6<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 6"    },
+      { "<A-7>", "<CMD>BufferGoto 7<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 7"    },
+      { "<A-8>", "<CMD>BufferGoto 8<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 8"    },
+      { "<A-9>", "<CMD>BufferGoto 9<CR>",       mode = {"n"}, desc = "[Buffer] Go to buffer 9"    },
+      { "<A-h>", "<CMD>BufferPrevious<CR>",     mode = {"n"}, desc = "[Buffer] Previous buffer"   },
+      { "<A-l>", "<CMD>BufferNext<CR>",         mode = {"n"}, desc = "[Buffer] Next buffer"       },
+      { "<A-c>", "<CMD>BufferClose<CR>",        mode = {"n"}, desc = "[Buffer] Close buffer"      },
+    },
+    opts = {
+      animation = false,
+      -- Automatically hide the tabline when there are this many buffers left.
+      -- Set to any value >=0 to enable.
+      auto_hide = 1,
+
+      -- Set the filetypes which barbar will offset itself for
+      sidebar_filetypes = {
+        NvimTree = true, -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+      },
+    },
+  },
+
+  -- 文件列表：nvim-tree.lua
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      { "<leader>e", "<CMD>NvimTreeToggle<CR>", mode = { "n" }, desc = "[NvimTree] Toggle NvimTree" },
+    },
+    opts = {
+      view = {
+        width = 30,
+        side = "left",
+      },
+      renderer = {
+        icons = {
+          show = {
+            git = true,
+            folder = true,
+            file = true,
+            folder_arrow = true,
+          },
+        },
+      },
+      filters = {
+        dotfiles = false,
+      },
+    },
+  },
+
+  -- 多彩括号：rainbow-delimiters.nvim
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    main = "rainbow-delimiters.setup",
+    submodules = false,
+    opts = {},
+  },
+
+  -- UI美化：noice.nvim
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "<leader>sN", "<CMD>Noice pick<CR>", desc = "[Noice] Pick history messages" },
+      { "<leader>N", "<CMD>Noice<CR>", desc = "[Noice] Show history messages" },
+    },
+    opts = {
+      popupmenu = {
+        enabled = false,
+      },
+      notify = {
+        enabled = false,
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+          ["vim.lsp.util.stylize_markdown"] = false,
+        },
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+      routes = {
+        -- Hide search count
+        { filter = { event = "msg_show", kind = "search_count" }, opts = { skip = true } },
+        -- Hide written message
+        { filter = { event = "msg_show", kind = "" }, opts = { skip = true } },
+      },
+    },
+  },
+}
+
