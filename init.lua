173b8790d1a9ba7918fe4fe8fc10788f8a421d6a
elf004 <1873475824@qq.com>
2025-12-05 13:58:33 +0800

    lemonade 在打开neovim的时候启动，ctrl ]快速跳出括号

--- init.lua
+++ init.lua
@@ -1,3 +1,36 @@
+-- 启动 lemonade server（若未运行）
+local lemonade_stderr = {}
+local lemonade_failed = false
+vim.fn.jobstart({ "lemonade", "server" }, {
+  detach = true,
+  on_stderr = function(_, data)
+    if data then
+      for _, line in ipairs(data) do
+        if line ~= "" then
+          table.insert(lemonade_stderr, line)
+        end
+      end
+    end
+  end,
+  on_exit = function()
+    lemonade_failed = true
+    vim.defer_fn(function()
+      local stderr_text = table.concat(lemonade_stderr, " "):lower()
+      if stderr_text:find("already") or stderr_text:find("in use") or stderr_text:find("bind") then
+        vim.notify("Lemonade server 已在运行", vim.log.levels.INFO)
+      else
+        vim.notify("Lemonade server 启动失败: " .. table.concat(lemonade_stderr, " "), vim.log.levels.ERROR)
+      end
+    end, 600)
+  end,
+})
+-- 延迟检查：如果进程没有退出，说明启动成功
+vim.defer_fn(function()
+  if not lemonade_failed then
+    vim.notify("Lemonade server 启动成功", vim.log.levels.INFO)
+  end
+end, 800)
+
 -- 设置代理环境变量，让 curl 能够使用代理
 -- HTTP 和 HTTPS 代理设置为本地 8118 端口
 vim.env.HTTP_PROXY = "http://127.0.0.1:8118"
